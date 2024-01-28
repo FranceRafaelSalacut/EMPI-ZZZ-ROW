0f066500df8813cdbd8b5adc9a5f43fbc09c023f
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-01-28 19:11:30 +0800

    added a main function and rewrote accept input

--- Salacut_MP0.py
+++ Salacut_MP0.py
@@ -1,7 +1,6 @@
 '''
 HONOR CODE
-I declare, upon my honor, that I did this machine problem assignment by myself
-using references from the lecture notes and MPs.
+I declare, upon my honor, that I did this machine problem assignment by myself using references from the lecture notes and MPs.
 
 REPO LINK
 https://github.com/FranceRafaelSalacut/EMPI-ZZZ-ROW.git
@@ -9,15 +8,11 @@ https://github.com/FranceRafaelSalacut/EMPI-ZZZ-ROW.git
 
 from icecream import ic #a library for easier debug
 
-#ic.disable()
+def main():
+    i = int(input())
+    ic(type(i))
 
-i = int(input())
-ic(type(i))
-arr = []
-ic(type(arr))
+    for _ in range(0, i):
+        ic(input())
 
-for _ in range(0, i):
-    arr.append(input())
-
-
-ic(arr)
+main()
