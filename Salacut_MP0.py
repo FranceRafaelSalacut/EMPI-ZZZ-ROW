56af34d4f1f0814bd6f2f399e533d57775a9ab4c
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-01-28 18:59:33 +0800

    added accept input functionality

--- Salacut_MP0.py
+++ Salacut_MP0.py
@@ -1,1 +1,14 @@
-print("Hello Worlddddasdasdasdasdasdasd")
+from icecream import ic #a library for easier debug
+
+ic.disable()
+
+i = int(input())
+ic(type(i))
+arr = []
+ic(type(arr))
+
+for _ in range(0, i):
+    arr.append(input())
+
+
+ic(arr)
