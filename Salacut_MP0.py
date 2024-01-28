7152d642e6c7ba584d1d63f87395d2583b5bb2fc
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-01-28 19:06:34 +0800

    added honor code and git link

--- Salacut_MP0.py
+++ Salacut_MP0.py
@@ -1,6 +1,15 @@
+'''
+HONOR CODE
+I declare, upon my honor, that I did this machine problem assignment by myself
+using references from the lecture notes and MPs.
+
+REPO LINK
+https://github.com/FranceRafaelSalacut/EMPI-ZZZ-ROW.git
+'''
+
 from icecream import ic #a library for easier debug
 
-ic.disable()
+#ic.disable()
 
 i = int(input())
 ic(type(i))
