d9b6b1adbbe48666dac9cec6282411f3d7dc19f0
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-01-29 13:15:03 +0800

    initial stages of check var declaration

--- Salacut_MP0.py
+++ Salacut_MP0.py
@@ -7,22 +7,39 @@ https://github.com/FranceRafaelSalacut/EMPI-ZZZ-ROW.git
 '''
 
 from icecream import ic #a library for easier debug
+ic.disable()
 
-def check_var_decleration(word):
-    ic()
-    ic(word)
+D_TYPE = ["int",
+        "char",
+        "float",
+        "double",
+        "void"
+        ]
 
-def check_fun_decleration(word):
+def check_var_declaration(tokens):
     ic()
-    ic(word)
+    ic(tokens)
+    
+    for token in tokens:
+        if "(" in token or ")" in token:
+            return "INVALID VARIABLE DECLARATION"
+    
+
+
+
+
+def check_fun_declaration(tokens):
+    ic()
+    ic(tokens)
 
 def main():
     i = int(input())
     ic(type(i))
 
     for _ in range(0, i):
-        div = input().split()
-        ic(div[0])
-        check_var_decleration(div[1:]) if div[0] == "1" else check_fun_decleration(div[1:])
+        tokens = input().split()
+        ic(tokens[0])
+        result = check_var_declaration(tokens[1:]) if tokens[0] == "1" else check_fun_declaration(tokens[1:])
+        print(result)
 
 main()
